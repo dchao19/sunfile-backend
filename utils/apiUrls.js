7f6f5cc3354111428926094c122705ff75b02736
Daniel Chao <daniel@danielchao.me>
2016-05-27 22:18:21 -0600

    External API data prep

---
+++ utils/apiUrls.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AUTHOR: "https://api.aylien.com/api/v1/extract",
+    SUMMARY: "https://api.aylien.com/api/v1/summarize",
+    TITLE: "http://gateway-a.watsonplatform.net/calls/url/URLGetTitle",
+    DATE: "http://gateway-a.watsonplatform.net/calls/html/HTMLGetPubDate",
+    TEXT: "http://gateway-a.watsonplatform.net/calls/html/HTMLGetText"
+}
