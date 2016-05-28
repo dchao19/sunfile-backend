33e4e288a0be70ee50cbc50fffcf43b3ee41a745
Daniel Chao <daniel@danielchao.me>
2016-05-27 22:18:39 -0600

    Starting work on backend data collection instead of front

---
+++ routes/mobile.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/featured_sources', function(req, res){
+     
+});
+
+
+/*
+
+{
+    international: {
+        label: "International Sources",
+        sources: [
+            {
+                title: "",
+                tags: [""],
+                detail: "",
+                short: "",
+                feedUrl: ""
+            }
+        ] 
+    }
+    
+    domestic: {
+        label: "Domestic Sources",
+        sources: [
+            {
+                title: "",
+                tags: [""],
+                detail: "",
+                short: "",
+                feedUrl: ""
+            }
+        ] 
+    }
+    
+    wires: {
+        label: "Wires and Aggregates",
+        sources: [
+            {
+                title: "",
+                tags: [""],
+                detail: "",
+                short: "",
+                feedUrl: ""
+            }
+        ] 
+    }
+}
+
+*/
+module.exports = router;
