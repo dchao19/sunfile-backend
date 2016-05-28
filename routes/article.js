b704f07922540a23025dc58df40633d2e2052715
Daniel Chao <daniel@danielchao.me>
2016-05-27 22:29:18 -0600

    article and apikeys are being fleshed out

---
+++ routes/article.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var unirest = require('unirest');
+var apiKeys = require('../utils/apiKeys')
+var router = express.Router();
+
+router.use(function (req, res, next) {
+    if(!req.session || !req.session.authenticated) res.json(401, {"message": "Unauthorized!", "errMessage" : "You are not logged in with the correct credientals to preform this action / your cookie could have expired!" });
+    else next();
+})
+
+router.post('/article_content', function (req, res) {
+    if(!req.body.htmlContent) res.json(400, { "message": "Missing data", "errMessage": "The htmlContent data is empty."});
+    else {
+        var req = unirest("POST", "http://gateway-a.watsonplatform.net/calls/html/HTMLGetText");
+
+        req.headers({
+            "content-type": "application/x-www-form-urlencoded"
+        });
+
+        req.form({
+            "apikey": apiKeys.alchemy,
+            "html": req.body.htmlContent,
+            "outputMode": "json"
+        });
+
+        req.end(function (res2) {
+            if (res2.error) res2.json(500, { "message": "Server error", "errMessage": "An unexpected server error has occured." });
+            else {
+               res.json({ "message": "success", "result": res2.body.text})
+            }  
+        });
+    }
+});
---
+++ utils/apiKeys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    alchemy: "21f7f90fb3ae48659e29e3f75ebbfe19a0e1df69",
+    aylien: {
+        appId: "57453c5f",
+        key: "a26bcf0c8edb28a2e9f3f3814d46b6ce"
+    }
+}
