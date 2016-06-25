928c64818f033b853ba29f4307de57d9be35f5b7
Daniel Chao <daniel@danielchao.me>
2016-06-25 09:16:26 -0600

    formatting changes with eslint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.send('./views/index.html');
 });
