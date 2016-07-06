2fd65a21d76d1b641d159010650d12cf1d7a2c61
Daniel Chao <daniel@danielchao.me>
2016-07-05 19:39:10 -0600

    Update DB config url

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,13 +1,10 @@
-﻿var connection_string = '127.0.0.1:27017/sunfile';
+﻿var connectionString = '127.0.0.1:27017/sunfile';
 // if OPENSHIFT env variables are present, use the available connection info:
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+if (process.env.SUNFILE_DB_PASSWORD) {
+    connectionString = process.env.SUNFILE_DB_USERNAME + ":" + process.env.SUNFILE_DB_PASSWORD + "@" +
+    'mongo.danielchao.me:27017/sunfile';
 }
 
 module.exports = {
-    "url": connection_string
+    url: connectionString
 };
