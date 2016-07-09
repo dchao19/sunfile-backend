1d4860e1e5add50ae318bb6223a202f4008b0230
Daniel Chao <daniel@danielchao.me>
2016-07-08 22:07:14 -0600

    Increase max html body content size

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ passport.deserializeUser(Account.deserializeUser());
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.text({type: 'html'}));
+app.use(bodyParser.text({type: 'html', limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(session({
