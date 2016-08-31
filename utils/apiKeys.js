e645741d5a496e0a2b0ec63a5e456cd4d464c8bb
Daniel Chao <daniel@danielchao.me>
2016-08-30 22:14:30 -0600

    Fix economist.

--- utils/apiKeys.js
+++ utils/apiKeys.js
@@ -5,4 +5,4 @@ module.exports = {
         key: "APP_KEY"
     },
     authUserUpdate: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIybk55ekFBSWhRaVRwZzhybnl3V3M1QzRQVHptNktKeCIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSJdfX0sImlhdCI6MTQ2OTEzNTYzMywianRpIjoiMWI4NzNhYmVmODVmNDAyZWE4NmI4YjJiZGIwMzU1NmUifQ.9c3JQrmjCrOv_gHf9fp8u75VWRaeNius6e-syHAsXVo"
-}
+};
