a8b2d7a5110cd50d6f04fd653fc6a7037b0edf59
Daniel Chao <daniel@danielchao.me>
2017-05-10 15:32:50 -0600

    Prepare to publish

--- utils/apiKeys.js
+++ utils/apiKeys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    alchemy: "21f7f90fb3ae48659e29e3f75ebbfe19a0e1df69",
+    alchemy: "ALCHEMY_API_KEY",
     aylien: {
-        appId: "57453c5f",
-        key: "a26bcf0c8edb28a2e9f3f3814d46b6ce"
+        appId: "APP_ID",
+        key: "APP_KEY"
     },
-    authUserUpdate: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIybk55ekFBSWhRaVRwZzhybnl3V3M1QzRQVHptNktKeCIsInNjb3BlcyI6eyJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJjcmVhdGUiLCJ1cGRhdGUiXX0sInVzZXJzIjp7ImFjdGlvbnMiOlsidXBkYXRlIl19fSwiaWF0IjoxNDgyNTI3OTU1LCJqdGkiOiJiYWVlY2NmYmZkMDFmZTk4NWIxOThjMDM2Y2JjMzYyZCJ9.RHgdgW0Oep596tqsbssFKvFAaQeMZ8-DMBm1TOU-fg8"
+    authUserUpdate: "AUTH_USER_UPDATE"
 };
