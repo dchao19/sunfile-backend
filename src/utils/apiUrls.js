58aa96436aef4cb49c77db1a40dc246dfdd87133
Daniel Chao <daniel@danielchao.me>
2018-07-22 18:53:42 -0600

    Fix article endpoint

--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -3,5 +3,6 @@ module.exports = {
     DATE: "https://api.aylien.com/api/v1/extract",
     TEXT: "https://gateway-a.watsonplatform.net/calls/html/HTMLGetText",
     COMBINED: "https://gateway-a.watsonplatform.net/calls/html/HTMLGetCombinedData",
-    USER: "https://danielchao.auth0.com/api/v2/users"
+    USER: "https://danielchao.auth0.com/api/v2/users",
+    AYLIEN_EXTRACTION: "https://api.aylien.com/api/v1/extract"
 };
