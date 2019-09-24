baf6ad14f790929bf832cdf8a7d3012960679a34
Daniel Chao <daniel@danielchao.me>
2019-09-24 11:04:43 -0700

    Add more pruning for WSJ articles

--- src/utils/articleUtils.js
+++ src/utils/articleUtils.js
@@ -7,7 +7,7 @@ const pruneHtml = htmlData => {
     var $ = require("jquery")(window);
     var preStrip = $("<div/>").html(htmlData);
     var htmlData = preStrip
-        .find(".last-update,.fyre,style,script,.layout-detail-page__footer,.teaser__byline")
+        .find(".last-update,.fyre,style,script,.layout-detail-page__footer,.teaser__byline,.full-header,.trending_now,.cx-join-the-conversation,.email-scrim-target,.wsj-footer")
         .remove()
         .end()
         .html();
